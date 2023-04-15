746d806f01264a1bf8e6ee510bee04c8a054cb77
Franco Lacamoire <francolacamoire@gmail.com>
2023-04-14 22:06:14 -0300

    se creo presupuesto.html presupuesto.css y se agregó funcionalidad al boton 'solicitar presupuesto' y al link 'ventas' del nav.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Evento click del burger-menu del header
 
-const btnBurger = document.querySelector(".burger-menu")
-const navOuterMenu = document.querySelector(".nav-outerList")
+const btnBurger = document.querySelector(".burger-menu");
+const navOuterMenu = document.querySelector(".nav-outerList");
 
 btnBurger.addEventListener("click", function(){
     navOuterMenu.classList.toggle("nav-outerList--visible");    // Si la clase existe entonces toggle la elimina, caso contrario la agrega
 })
+
+
+
+// Evento click del botón Solicitar Presupuesto de la página index.html
+
+const btnPresupuesto = document.querySelector("#btnPresupuesto");
+
+
+btnPresupuesto.addEventListener("click", function() {
+    window.location.href = "páginas/presupuesto.html";
+});
